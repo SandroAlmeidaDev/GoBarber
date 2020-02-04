82bd0268c2e3e6f55a0f67005b7bee960ee2bfb3
SandroAlmeidaDev <sandroalmeida25@hotmail.com>
2020-02-04 01:19:25 -0300

    Criado configuracao com banco postgres, migration e model de users

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello Word' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Sandro Almeida',
+    email: 'sandro.almeida.silva17@gmail.com',
+    password_hash: '12345678',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
