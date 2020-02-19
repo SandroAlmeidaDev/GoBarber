00c8941cc32d44d59fa04bad7af0a3b6ee777be5
SandroAlmeidaDev <sandroalmeida25@hotmail.com>
2020-02-19 01:05:55 -0300

    Configuracao de envio de email nodemailer

---
+++ backend/src/config/mail.js
@@ -0,0 +1,18 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '28e30223a2f317',
+    pass: '964aac2bf5a11b',
+  },
+  default: {
+    from: 'Equipe Gobarber <noreply@gobarber.com>',
+  },
+};
+
+/**
+ * Servicos de envio de email:
+ * Amazon SES, Mailgun, Sparkpost, Mandril(Mailchimp)
+ * Mailtrap (DEV)
+ */
